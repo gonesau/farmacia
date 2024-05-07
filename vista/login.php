d14cdc715e40863ac2c476985f6affa8f143778c
gonesau <gonesau20@gmail.com>
2024-05-07 16:37:48 -0600

    Primer Commit

---
+++ vista/login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/css/all.min.css">
+    <title>Login</title>
+</head>
+<body>
+    <img class="wave" src="../img/wave.png" alt="">
+    <div class="contenedor">
+        <div class="img">
+            <img src="../img/farmacia.svg" alt="">
+        </div>
+        <div class="contenido-login">
+            <form action="">
+                <img src="../img/medicine.png" alt="">
+                <h2>Farmacia</h2>
+                <div class="input-div dui">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="div">
+                        <h5>DUI</h5>
+                        <input type="text" name="user" class="input">
+                    </div>
+                </div>
+                <div class="input-div pass">
+                    <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Contraseña</h5>
+                        <input type="password" name="pass" class="input">
+                    </div>
+                </div>
+                <a href="">Created by Ubuntomar</a>
+                <input type="submit" class="btn" value="Iniciar Sesion">
+            </form>
+        </div>
+    </div>
+</body>
+<script src="../js/login.js"></script>
+</html>
