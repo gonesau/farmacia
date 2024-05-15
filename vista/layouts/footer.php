865d97ff53c52dc771e76153927c121bf2d72aad
gonesau <gonesau20@gmail.com>
2024-05-15 08:16:02 -0600

    Edición Footer

--- vista/layouts/footer.php
+++ vista/layouts/footer.php
@@ -1,9 +1,8 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.0.4
+      <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
-    reserved.
+    <strong>Copyright &copy; 2024 <a href="https://github.com/gonesau/farmacia">FarmaNET</a>.</strong> Creado por UDev
   </footer>
 </div>
 <!-- ./wrapper -->
