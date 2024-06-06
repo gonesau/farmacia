99a8f63e92003585699569b729472313b6a66b45
gonesau <gonesau20@gmail.com>
2024-06-05 22:21:20 -0600

    Correción extensión pdf

--- controlador/PDFController.php
+++ controlador/PDFController.php
@@ -5,6 +5,6 @@ $id_venta = $_POST['id'];
 $mpdf = new \Mpdf\Mpdf();
 $mpdf->WriteHTML('<h1>Hello world!</h1>');
 
-$mpdf->Output("../pdf/pdf-" . $id_venta . "pdf", "F");
+$mpdf->Output("../pdf/pdf-" . $id_venta . ".pdf", "F");
 
 ?>
